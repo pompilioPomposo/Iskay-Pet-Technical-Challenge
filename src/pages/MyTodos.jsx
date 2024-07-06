2ee127d0191af397b1099e0460ab8fdec01955c3
pompilioPomposo <mauro.monterotula@gmail.com>
2024-07-06 13:12:02 -0300

    modal styling

--- src/pages/MyTodos.jsx
+++ src/pages/MyTodos.jsx
@@ -1,11 +1,42 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../AppContext';
-import { Text, Box, Button } from '@chakra-ui/react';
+import {
+  Text,
+  Box,
+  Button,
+  HStack,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  FormControl,
+  FormLabel,
+  Input,
+  Textarea,
+} from '@chakra-ui/react';
 import ToDo from '../components/ToDo';
-import { boxStyles, textStyles, buttonStyles } from '../styles/MyTodosStyles';
+import {
+  boxStyles,
+  textStyles,
+  modalHeaderStyles,
+  formControlStyles,
+  buttonStyles,
+  cancelButtonStyles,
+  saveButtonStyles,
+  descriptionInputStyles,
+} from '../styles/MyTodosStyles';
+import { inputStyles, labelStyles } from '../styles/PersonalDataStyles';
 
 const MyTodos = () => {
   const { selectedTab } = useContext(AppContext);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const saveTodo = async () => {
+    // state management here
+  };
 
   if (selectedTab !== 'Mis tareas') return null;
 
@@ -15,7 +46,36 @@ const MyTodos = () => {
       <ToDo />
       <ToDo />
       <ToDo />
-      <Button {...buttonStyles}>Añadir tarea</Button>
+      <Button {...buttonStyles} onClick={onOpen}>
+        Añadir tarea
+      </Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent mx='1.0625rem'>
+          <ModalHeader {...modalHeaderStyles}>Añadir tarea</ModalHeader>
+          <ModalCloseButton pt='25px' />
+          <ModalBody>
+            <FormControl {...formControlStyles}>
+              <FormLabel {...labelStyles}>Nombre</FormLabel>
+              <Input placeholder='Nombre' {...inputStyles} />
+            </FormControl>
+            <FormControl {...formControlStyles}>
+              <FormLabel {...labelStyles}>Descripción</FormLabel>
+              <Textarea placeholder='Descripción' {...descriptionInputStyles} />
+            </FormControl>
+          </ModalBody>
+
+          <HStack justifyContent='space-between'>
+            <Button variant='ghost' onClick={onClose} {...cancelButtonStyles}>
+              Cancelar
+            </Button>
+            <Button onClick={saveTodo} {...saveButtonStyles}>
+              Guardar
+            </Button>
+          </HStack>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 };
--- src/styles/MyTodosStyles.js
+++ src/styles/MyTodosStyles.js
@@ -18,6 +18,24 @@ export const textStyles = {
   alignSelf: 'flex-start',
 };
 
+export const modalHeaderStyles = {
+  pt: '1.5625rem',
+  pb: '2rem',
+  fontSize: '1.25rem',
+  fontWeight: '700',
+  lineHeight: '1.7025rem',
+  color: '#333333',
+  alignSelf: 'flex-start',
+};
+
+export const formControlStyles = {
+  pt: '0rem',
+  pb: '1.5rem',
+  fontSize: '0.875rem',
+  fontWeight: '400',
+  lineHeight: '1.191875rem',
+};
+
 export const buttonStyles = {
   width: '100%',
   height: '2.875rem',
@@ -25,9 +43,49 @@ export const buttonStyles = {
   background: '#639605',
   color: '#FFFFFF',
   my: '2.625rem',
-  p: '0.75rem 8.8125rem 0.75rem 8.8125rem',
+  p: '0.75rem 8.8125rem',
   fontSize: '1rem',
   fontWeight: '600',
   lineHeight: '1.361875rem',
   gap: '0.625rem',
+};
+
+export const cancelButtonStyles = {
+  variant: 'ghost',
+  height: '2.875rem',
+  width: '45%',
+  borderRadius: '0.3125rem',
+  ms: '1.5rem',
+  mb: '2.625rem',
+  p: '0.75rem 0',
+  fontSize: '1rem',
+  fontWeight: '600',
+  lineHeight: '1.361875rem',
+  gap: '0.625rem',
+};
+
+export const saveButtonStyles = {
+  height: '2.875rem',
+  width: '45%',
+  borderRadius: '0.3125rem',
+  background: '#639605',
+  color: '#FFFFFF',
+  me: '1.5rem',
+  mb: '2.625rem',
+  p: '0.75rem 0',
+  fontSize: '1rem',
+  fontWeight: '600',
+  lineHeight: '1.361875rem',
+  gap: '0.625rem',
+};
+
+export const descriptionInputStyles = {
+  height: '9.75rem',
+  color: '#4A4A4A',
+  border: '0.0625rem solid #C9C9C9',
+  borderRadius: '0.1875rem',
+  _focus: {
+    borderColor: '#C9C9C9',
+    boxShadow: 'none',
+  },
 };
