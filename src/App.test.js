6507cffde09f8af69fd5cff0b62bc871dee4d4b2
pompilioPomposo <mauro.monterotula@gmail.com>
2024-07-06 22:33:24 -0300

    more test file changes

--- src/App.test.js
+++ src/App.test.js
@@ -3,15 +3,16 @@ import { render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import MyTodos from './pages/MyTodos';
 import { AppContext } from './AppContext';
+import { fetchTodos } from './services/todoService';
 
-jest.mock('./services/todoService', () => ({
-  fetchTodos: jest.fn(() =>
-    Promise.resolve([
-      { name: 'Dummy', description: 'Tarea inicial' },
-      { name: 'Dummy 2', description: 'Tarea secundaria' },
-    ])
-  ),
-}));
+jest.mock('./services/todoService');
+
+const mockTodos = [
+  { name: 'Dummy', description: 'Tarea inicial' },
+  { name: 'Dummy 2', description: 'Tarea secundaria' },
+];
+
+fetchTodos.mockResolvedValue(mockTodos);
 
 test('fetches and displays todos', async () => {
   const contextValue = { selectedTab: 'Mis tareas' };
