cc33309ee5101cd663e1afbaacf25a6eb8f1127c
pompilioPomposo <mauro.monterotula@gmail.com>
2024-07-06 22:11:04 -0300

    a more complex test

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,29 @@
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import MyTodos from '../pages/MyTodos';
+import { AppContext } from '../AppContext';
 
-test('renders without crashing', () => {
-  render(<App />);
+jest.mock('../services/todoService', () => ({
+  fetchTodos: jest.fn(() =>
+    Promise.resolve([
+      { name: 'Dummy', description: 'Tarea inicial' },
+      { name: 'Dummy 2', description: 'Tarea secundaria' },
+    ])
+  ),
+}));
+
+test('fetches and displays todos', async () => {
+  const contextValue = { selectedTab: 'Mis tareas' };
+
+  render(
+    <AppContext.Provider value={contextValue}>
+      <MyTodos />
+    </AppContext.Provider>
+  );
+
+  await waitFor(() => {
+    expect(screen.getByText('Tarea inicial')).toBeInTheDocument();
+    expect(screen.getByText('Tarea secundaria')).toBeInTheDocument();
+  });
 });
